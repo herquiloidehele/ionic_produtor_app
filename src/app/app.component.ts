669f22f794ff174ad0110bd41d5b2f785925993c
Herquiloide Hele <herquiloide@gmail.com>
2019-01-23 18:06:57 +0200

    inicial page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,11 @@ import {Component, ViewChild} from '@angular/core';
 import {AlertController, Events, Platform} from 'ionic-angular';
 import {App} from 'ionic-angular/components/app/app'
 import {LoginPage} from "../pages/login/login";
-import {AutenticacaoProvider} from "../providers/autenticacao/autenticacao";
 import {ProdutosrequsitadosPage} from "../pages/modulo-produtor/produtosrequsitados/produtosrequsitados";
 import {DisponibilizarProdutosPage} from "../pages/modulo-produtor/disponibilizar-produtos/disponibilizar-produtos";
 import {ProdutosDisponibilizadosPage} from "../pages/modulo-produtor/produtos-disponibilizados/produtos-disponibilizados";
 import {PerfilPage} from "../pages/perfil/perfil";
-import {Network} from "@ionic-native/network";
-import {UrlapiProvider} from "../providers/urlapi/urlapi";
-import {UserInfoPage} from "../pages/user-info/user-info";
-import {LocalizacaoPage} from "../pages/localizacao/localizacao";
-import {EscolherProdutoPage} from "../pages/escolher-produto/escolher-produto";
 import {StartPage} from "../pages/start/start";
-import {ShowEscolherProdutosPage} from "../pages/show-escolher-produtos/show-escolher-produtos";
 
 @Component({
   templateUrl: 'app.html'
@@ -60,7 +53,7 @@ export class MyApp {
 
 
    getPage() {
-      this.rootPage = ShowEscolherProdutosPage;
+      this.rootPage = StartPage;
    }
 
 
